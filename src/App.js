e736ba596f949d6b950ac25beab3d5e180c819c9
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-07-19 21:12:37 +0300

    Create basic structure for Rockets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Rockets />} />
+        <Route path="/profile" element={<Rockets />} />
+      </Routes>
     </div>
   );
 }
